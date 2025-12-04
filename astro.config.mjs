2d97c3eedc1eb781fd667f8f7c73838985fc0b0e
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2025-12-04 01:26:10 -0300

    feat: add delete functionality for historias and refactor image upload logic

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,20 @@ import tailwindcss from '@tailwindcss/vite';
 
 import node from '@astrojs/node';
 
+import netlify from '@astrojs/netlify';
+import { loadEnv } from 'vite'; // <--- Importante
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// Importante para poder actualizar fácil la URL del backend
+
+// 1. Cargar las variables de entorno actuales (según el modo 'production' o 'development')
+const { PUBLIC_SUPABASE_URL } = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), '');
+
+// 2. Limpiar la URL para obtener solo el dominio
+// Si existe la URL, le quita 'https://' y la barra final. Si no, devuelve null.
+const supabaseDomain = PUBLIC_SUPABASE_URL 
+  ? PUBLIC_SUPABASE_URL.replace('https://', '').replace(/\/$/, '')
+  : "";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,11 +28,9 @@ export default defineConfig({
   // Sin esto, Astro sigue generando archivos estáticos y los formularios no funcionarán.
   output: 'server',
   image:{
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com', supabaseDomain].filter(Boolean), // Filtra null si no hay dominio
   },
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: netlify(),
 
   vite: {
     plugins: [tailwindcss()],
